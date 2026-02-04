7427e5891547c8282a671ab56e076a50ac0347b2
zoeycodefun <zoeycodefun@gmail.com>
2026-02-04 23:13:19 +0800

    update 2020.2.4

---
+++ src/layout/layout_components/pages_navigation.tsx
@@ -0,0 +1,7 @@
+// page navigation(导航组件)
+import React from 'react';
+import { Link, useLocation} from 'react-router-dom';
+import { categories } from '../../data/products';
+import { type Category } from '../../types/interface';
+
+// define the interfaces of the 
